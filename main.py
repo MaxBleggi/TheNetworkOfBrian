8c9d642e7d65da160900d095c844d16fefb240f8
Max Bleggi <bleggi7@gmail.com>
2019-06-07 01:17:13 -0400

    finished poping stash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from mnist_loader import load_formatted
 from vanilla_network import VanillaNetwork
+from vanilla_network_iterative import VanillaNetworkIterative
+import json
 
 MNIST_PATH = 'data/mnist.pkl.gz'
 
@@ -7,10 +9,36 @@ MNIST_PATH = 'data/mnist.pkl.gz'
 def main():
     training_load, validation_load, test_load = load_formatted(MNIST_PATH)
 
-    # input layer must remain at 784 as to correspond to 28x28 pixels of the image
-    net = VanillaNetwork([784, 30, 10])
-    data = net.stoch_gradient_descent(training_load, 10, 10, 3.0, test_data=test_load)
-    print(data)
+    # [ |input_layer| , ... |hidden layer| ..., |output_layer| ]
+    nnet_input = [784, 30, 10]
+
+    # hyper-parameters
+    epochs = 30
+    batch_size = 10
+    learning_rate = 3.0
+
+    # note: ``stoch_gradient_descent()`` will return {} if not test_data is provided
+
+    vanilla_net = VanillaNetwork(nnet_input)
+    data_set_vn = vanilla_net.stoch_gradient_descent(
+        training_load,
+        epochs,
+        batch_size,
+        learning_rate,
+        test_data=test_load
+    )
+
+    vanilla_net_iterative = VanillaNetworkIterative(nnet_input)
+    data_set_vni = vanilla_net_iterative.stoch_gradient_descent(
+        training_load,
+        epochs,
+        batch_size,
+        learning_rate,
+        test_data=test_load
+    )
+
+    with open('docs/nnet_output', 'w') as file:
+        json.dump(data_set_vn, file)
 
 
 if __name__ == '__main__':
